9114e6ce7af92d286b6b29ea379cd791ada45a95
Ali Sherkat <63319613+alisherkat@users.noreply.github.com>
2023-12-11 18:52:53 +0330

    Update wsgi.py

--- enaba/wsgi.py
+++ enaba/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
+from pathlib import Path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'enaba.settings')
 BASE_DIR = Path(__file__).resolve().parent.parent
