a41ec4b0dcf28128f650a253a76461403f77836b
Ali Sherkat <63319613+alisherkat@users.noreply.github.com>
2023-12-11 16:47:17 +0330

    Update urls.py

--- enaba/urls.py
+++ enaba/urls.py
@@ -22,10 +22,10 @@ from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # re_path(r'زیارت/(?P<slug>[^/]+)/$', views.post_view, name="post_view_zyarat"),
-    # re_path(r'دعا/(?P<slug>[^/]+)/$', views.post_view, name="post_view_doa"),
-    # re_path(r'فهرست/(?P<slug>[^/]+)/$', views.content_view, name="content_view"),
-    # path('', views.main_page, name="main_page")
+    re_path(r'زیارت/(?P<slug>[^/]+)/$', views.post_view, name="post_view_zyarat"),
+    re_path(r'دعا/(?P<slug>[^/]+)/$', views.post_view, name="post_view_doa"),
+    re_path(r'فهرست/(?P<slug>[^/]+)/$', views.content_view, name="content_view"),
+    path('', views.main_page, name="main_page")
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
