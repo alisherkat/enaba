ad4b72250f12851b296fc2b6b01a8a0b757a3dc5
Ali Sherkat <63319613+alisherkat@users.noreply.github.com>
2023-12-11 18:46:04 +0330

    Update wsgi.py

--- enaba/wsgi.py
+++ enaba/wsgi.py
@@ -8,10 +8,13 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'enaba.settings')
-
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 application = get_wsgi_application()
+application = WhiteNoise(application, root=STATIC_ROOT)
+
 app = application
