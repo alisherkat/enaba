71c2ded17b477717f7758fc859169e868d707cf5
Ali Sherkat <63319613+alisherkat@users.noreply.github.com>
2023-12-11 11:02:42 +0330

    Update build_file.sh

--- build_file.sh
+++ build_file.sh
@@ -1,5 +1,6 @@
 # build_files.sh
-
 pip install -r requirements.txt
-python manage.py migrate
-python manage.py collectstatic 
+
+# make migrations
+python3.9 manage.py migrate 
+python3.9 manage.py collectstatic
